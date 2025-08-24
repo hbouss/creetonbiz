7b322fd0130eef6bfba1a2cdb802d2b1bd3c4e51
hboussouar <hichem.boussouar@gmail.com>
2025-08-24 11:29:28 +0200

    reglage style  et qualité formulaire et reponse, reglage du rating en cours, business plan elaboré, projet finis, deployement, test deploy, 2, 3, 4, 5, deploy frontend ajout _redirects réajout playwright, 6, test final

--- backend/main.py
+++ backend/main.py
@@ -19,9 +19,15 @@ from backend.routers.ideas import router as ideas_router
 from backend.services.deliverable_service import STORAGE_DIR
 
 app = FastAPI()
+
+ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://creetonbiz.netlify.app",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
